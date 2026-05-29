a3bf510af28659320d32718663e0b9159e501c28
Daniel Ching <dching@nvidia.com>
2026-05-29 17:58:19 -0500

    REF: Build from source and use v1 recipe format
    Co-authored-by: Isuru Fernando <ifernando@openteams.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,44 +1,94 @@
 #!/bin/bash
 
-# Install to conda style directories
-[[ -d lib64 ]] && mv lib64 lib
+# Copy of https://github.com/conda-forge/gdb-feedstock/blob/main/recipe/build.sh
 
-[[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
-[[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
-[[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "sbsa" ]] && targetsDir="targets/sbsa-linux"
-[[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "tegra" ]] && targetsDir="targets/aarch64-linux"
+# Get an updated config.sub and config.guess
+cp "$BUILD_PREFIX"/share/gnuconfig/config.* ./readline/readline/support
+cp "$BUILD_PREFIX"/share/gnuconfig/config.* .
 
-mkdir -p "${PREFIX}/${targetsDir}"
-mv -v extras/Debugger/include "${PREFIX}/${targetsDir}"
+set -eux
 
-# No python support for the debugger in non-x86 platforms on CUDA 12.9
-if [[ ${target_platform} == "linux-64" ]]; then
-    rm bin/cuda-gdb
-    if [[ ${PY_VER:-0} == "0" || ${skip_python} == "true" ]]; then
-        mv -v "bin/cuda-gdb-minimal" bin/cuda-gdb
-    else
-        # Due to an issue similar to https://github.com/astral-sh/python-build-standalone/issues/197,
-        # we need to strip an unneeded link to libcrypt.so.1
-        patchelf --remove-needed libcrypt.so.1 "bin/cuda-gdb-python${PY_VER}-tui"
-        mv -v "bin/cuda-gdb-python${PY_VER}-tui" bin/cuda-gdb
-    fi
-fi
+# Download the right script to debug python processes.
+# This is an useful script provided by CPython project to help debugging
+# crashes in Python processes.
+# See https://devguide.python.org/gdb for some
+# examples on how to use it.
+#
+# Normally someone needs to download this script manually and properly
+# setup gdb to load it (if you are lucky gdb was compiled with python
+# support).
+#
+# Providing this in conda-forge's gdb makes the experience much smoother,
+# avoiding all the hassles someone can find when trying to configure gdb
+# for that.
+curl -SL "https://raw.githubusercontent.com/python/cpython/$PY_VER/Tools/gdb/libpython.py" \
+    > "$SP_DIR/gdb_libpython.py"
 
-for i in `ls`; do
-    [[ $i == "build_env_setup.sh" ]] && continue
-    [[ $i == "conda_build.sh" ]] && continue
-    [[ $i == "metadata_conda_debug.yaml" ]] && continue
+# Install a gdbinit file that will be automatically loaded
+mkdir -p "$PREFIX/etc"
+echo '
+python
+import gdb
+import sys
+import os
+def setup_python(event):
+    import gdb_libpython
+gdb.events.new_objfile.connect(setup_python)
+end
+' >> "$PREFIX/etc/gdbinit"
 
-    if [[ $i == "bin" ]]; then
-        for j in `ls "${i}"`; do
-            [[ -f "bin/${j}" ]] || continue
+export CPPFLAGS="$CPPFLAGS -I$PREFIX/include -I${SRC_DIR}/include"
+export CXXFLAGS="${CXXFLAGS} -std=gnu++17"
 
-            echo patchelf --force-rpath --set-rpath "\$ORIGIN/../lib:\$ORIGIN/../${targetsDir}/lib" "${i}/${j}" ...
-            patchelf --force-rpath --set-rpath "\$ORIGIN/../lib:\$ORIGIN/../${targetsDir}/lib" "${i}/${j}"
-        done
-    fi
+# Setting /usr/lib/debug as debug dir makes it possible to debug the system's
+# python on most Linux distributions
 
-    cp -rv $i ${PREFIX}
-done
+mkdir build
+cd build
 
-check-glibc "$PREFIX"/lib*/*.so.* "$PREFIX"/bin/* "$PREFIX"/targets/*/lib*/*.so.* "$PREFIX"/targets/*/bin/*
+# --with-gdb-datadir is given to not conflict with gdb conda package
+#
+# --enable-targets=m68k-linux-gnu is required: cuda-gdb re-uses bfd_arch_m68k as the CUDA
+# architecture (see gdb/arch-utils.c around the NVIDIA_CUDA_GDB block), so libbfd must
+# include m68k cpu support or cuda-gdb aborts at startup with "Attempt to register unknown
+# architecture (2)". As of June 2026, only x86 and ARM platforms are supported host
+# platforms for CUDA, so we don't need to build for others.
+#
+# shellcheck disable=SC2016  # $debugdir/$datadir are literal gdb runtime tokens
+"$SRC_DIR"/configure \
+    --build="$BUILD" --host="$HOST" --target="$HOST" \
+    --disable-binutils \
+    --disable-gas \
+    --disable-gold \
+    --disable-gprof \
+    --disable-gprofng \
+    --disable-ld \
+    --disable-nls \
+    --disable-sim \
+    --disable-source-highlight \
+    --disable-werror \
+    --enable-cuda \
+    --enable-targets="x86_64-linux-gnu,aarch64-linux-gnu,m68k-linux-gnu" \
+    --enable-tui \
+    --prefix="$PREFIX" \
+    --program-prefix="cuda-" \
+    --with-auto-load-dir='$debugdir:$datadir/auto-load:/usr/share/gdb/auto-load' \
+    --with-curses \
+    --with-expat=yes \
+    --with-gdb-datadir="$PREFIX/share/cuda-gdb" \
+    --with-gmp="$PREFIX" \
+    --with-libiconv-prefix="$PREFIX" \
+    --with-lzma --with-liblzma-prefix="$PREFIX" \
+    --with-mpfr="$PREFIX" \
+    --with-pkgversion="conda-forge cuda-gdb" \
+    --with-python="$PYTHON" \
+    --with-separate-debug-dir="$PREFIX/lib/debug:/usr/lib/debug" \
+    --with-system-readline \
+    --with-system-gdbinit="$PREFIX/etc/gdbinit" \
+    --with-system-zlib \
+    --with-zstd=yes \
+    --without-guile \
+    || (cat config.log && exit 1)
+
+make -j"${CPU_COUNT}" VERBOSE=1
+make install install-gdbserver
